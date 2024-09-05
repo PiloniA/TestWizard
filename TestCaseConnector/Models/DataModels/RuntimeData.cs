f37b8b5da8f7a5df71c5fdd2f3503d29adb8400b
PiloniA <piloni@live.at>
2024-09-05 14:23:22 +0200

    remove unsused usings

--- TestCaseConnector/Models/DataModels/RuntimeData.cs
+++ TestCaseConnector/Models/DataModels/RuntimeData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AzureDevOpsTestConnector.Models.DataModels
+﻿namespace AzureDevOpsTestConnector.Models.DataModels
 {
     public class RuntimeData
     {
